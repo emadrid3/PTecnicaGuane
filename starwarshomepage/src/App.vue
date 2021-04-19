b5f81f69db537921bf5a45a4dd258104d010bfb8
Emanuel Madrid Restrepo <emanuel7233@gmail.com>
2021-04-19 18:44:38 -0500

    Añadiendo prueba tecnica

--- starwarshomepage/src/App.vue
+++ starwarshomepage/src/App.vue
@@ -1,32 +1,84 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <img alt="Vue logo" src="./assets/Star_Wars_logo.png" width="400">
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return {
+      avatars: []
     }
   }
+}
+</script>
+
+<style lang="scss">
+
+@font-face{
+  font-family: 'Starjedi';
+  src: url(./assets/fonts/Starjedi.ttf) format("truetype");;
+}
+
+
+#app {
+  text-align: center;
+  color: #2c3e50;
+  min-height: 100vh;
+  background-image: url("./assets/starwars.jpeg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+
+  img{
+    padding: 70px;
+    padding-bottom: 0px;
+  }
+}
+
+html {
+  width:100%;
+  height:100%;
+}
+
+body{
+  margin: 0px;
+}
+
+h1{
+  //-webkit-text-stroke: 2px red;
+  font-family: 'Starjedi';
+}
+h2{
+  //-webkit-text-stroke: 2px red;
+  font-family: 'Starjedi';
+}
+h3{
+  //-webkit-text-stroke: 2px red;
+  font-family: 'Starjedi';
+}
+h4{
+  //-webkit-text-stroke: 2px red;
+  font-family: 'Starjedi';
+}
+h5{
+  //-webkit-text-stroke: 2px red;
+  font-family: 'Starjedi';
+}
+h6{
+  //-webkit-text-stroke: 2px red;
+  font-family: 'Starjedi';
+}
+
+p{
+  //-webkit-text-stroke: 2px red;
+  font-family: 'Starjedi';
 }
 </style>
--- starwarshomepage/src/store/index.js
+++ starwarshomepage/src/store/index.js
@@ -1,14 +1,77 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios";
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    avatars : [],
+    avatarSelect : null
   },
   mutations: {
+    changeAvatars(state, avatarsin) {
+      state.avatars = avatarsin
+    },
+    changeAvatarSelect(state, avatarsel) {
+      state.avatarSelect = avatarsel
+    }
+  },
+  getters: {
+    getAvatars: state => state.avatars,
+    getAvatarSelect: state => state.avatarSelect
   },
   actions: {
+    async consumeAvatars({commit}, page){
+
+      let resultPeople = await axios.get("https://swapi.dev/api/people/?page="+page);
+
+      for (let index = 0; index < resultPeople.data.results.length; index++) {
+          
+        await axios.get(resultPeople.data.results[index].homeworld).then((result) => {
+          resultPeople.data.results[index].homeworld_name = result.data.name;
+        });
+        
+      }
+
+      commit("changeAvatars", resultPeople.data);
+    },
+
+    async consumeAvatarId({commit}, id){
+
+      let resultPeople = await axios.get("https://swapi.dev/api/people/"+id);
+
+      resultPeople.data.films_name = [];
+      resultPeople.data.starships_name = [];
+      resultPeople.data.vehicles_name = [];
+
+      for (let index = 0; index < resultPeople.data.films.length; index++) {
+          
+        await axios.get(resultPeople.data.films[index]).then((result) => {
+          resultPeople.data.films_name.push(result.data.title);
+        });
+        
+      }
+
+      for (let index = 0; index < resultPeople.data.vehicles.length; index++) {
+          
+        await axios.get(resultPeople.data.vehicles[index]).then((result) => {
+          resultPeople.data.vehicles_name.push(result.data.name);
+        });
+        
+      }
+
+      for (let index = 0; index < resultPeople.data.starships.length; index++) {
+          
+        await axios.get(resultPeople.data.starships[index]).then((result) => {
+          resultPeople.data.starships_name.push(result.data.name);
+        });
+        
+      }
+
+      commit("changeAvatarSelect", resultPeople.data);
+    }
   },
   modules: {
   }
